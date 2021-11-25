3950707e74cc3c707ba7cd26af8d5a07c9f9052d
Yansong Wang <61701525+rock1215@users.noreply.github.com>
2021-11-26 00:43:00 +0900

    Complete styled native

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar as ExpStatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
-      <ExpStatusBar style="auto" />
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,33 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { StatusBar, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-export const RestaurantScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar />
-      </View>
-      <View style={styles.list}>
-        <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
-  );
-};
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "white",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
